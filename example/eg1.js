8edf108a501e144a826e2c666154a3fbf505f35f
zhuqiuyang <zqyhit@qq.com>
2018-05-09 17:01:39 +0800

    update to 0.0.2 tst ok

---
+++ example/eg1.js
@@ -0,0 +1,11 @@
+const SYNC = {};
+
+SYNC.root = [
+  { TYPE: 'QUERY', storeName: 'query' },
+  { TYPE: 'IS_WD', storeName: 'is_wd' },
+  { TYPE: 'P_CODE', storeName: 'pCode' }
+];
+
+const config = {
+  SYNC
+};
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // CONSTANT key
 // CONSTANT NAME
-export default (moduleGenertor = (CONSTANT, CKEY) => {
+
+import Constant from './lib/Constant';
+import Mutation from './lib/Mutation';
+
+const moduleGenertor = (config, NAME, cKey) => {
+  if (!cKey) throw Error('Must give a config key');
+  const CONSTANT = new Constant(config, NAME, cKey);
   const module = {
     mutations: new Mutation({
-      CONSTANT,
-      CKEY
+      CONSTANT
     })
   };
-});
+};
+
+export default moduleGenertor;
