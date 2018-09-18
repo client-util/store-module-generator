d76afce142e33182d5838d3c82718ca260916a91
zhuqiuyang <zqyhit@qq.com>
2018-09-18 12:07:14 +0800

    add concat mutation. upgrade to 0.0.5

--- example/eg1.js
+++ example/eg1.js
@@ -1,14 +1,15 @@
-const mg = require('../index');
+const mg = require('../index')
 
-const SYNC = {};
+const SYNC = {}
 
 SYNC.root = [
-  { TYPE: 'FOO', storeName: 'foo' },
+  // arrayPush 表示, mutation 多一个 push更新的方法, 用于数据类型数据
+  { TYPE: 'FOO', storeName: 'foo', opts: { concat: true } },
   { TYPE: 'BAR', storeName: 'bar' }
-];
+]
 
 const config = {
   SYNC
-};
+}
 
-console.log(mg(config, 'TEST', 'root'));
+console.log(mg(config, 'TEST', 'root'))
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 // CONSTANT key
 // CONSTANT NAME
 
-const Constant = require('./lib/Constant');
-const Mutation = require('./lib/Mutation');
+const Constant = require('./lib/Constant')
+const Mutation = require('./lib/Mutation')
 
-const moduleGenertor = (config, NAME, cKey) => {
-  if (!cKey) throw Error('Must give a config key');
-  const CONSTANT = new Constant(config, NAME, cKey);
+const moduleGenertor = (config, ACTION_NAME, cKey) => {
+  if (!cKey) throw Error('Must give a config key')
+  if (!config.SYNC) throw Error('Must give a SYNC Object in config')
+  const CONSTANT = new Constant(config, ACTION_NAME, cKey)
   const module = {
+    // 参照 VUEX store muation 的配置
+    // https://vuex.vuejs.org/guide/mutations.html
     mutations: new Mutation({
       CONSTANT
     })
-  };
-  return module;
-};
+  }
+  return module
+}
 
-module.exports = moduleGenertor;
+module.exports = moduleGenertor
